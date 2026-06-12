d9eaa390e08d29e239d878a349977cfef75968cc
Niklas Meyer <niklasbhv@gmail.com>
2026-06-12 23:01:09 +0200

    Restructured project and added initial sketch for UI

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,6 +1,6 @@
 /**
  *  Copyright (C) 2026 Niklas Meyer
- * 
+ *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
@@ -9,11 +9,20 @@
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-int main(int argc, char *argv[]) {
-    return 0;
+#include <QApplication>
+#include "MainWindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    MainWindow window;
+    window.show();
+
+    return app.exec();
 }
